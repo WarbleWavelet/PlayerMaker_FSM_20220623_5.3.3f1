d3f5ce090f9ec92b9e033442042077956534b2cb
WWS <3027565221@qq.com>
2022-06-26 18:26:43 +0800

    仅两章

---
+++ PlayerMaker_FSM_20220623_5.3.3f1/Assets/Scenes/第二章/Bird.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bird : MonoBehaviour {
+
+
+    Rigidbody2D rgb2d;
+    public PlayMakerFSM fsm;
+    bool isGameOver = false;
+
+    void Awake()
+    {
+        rgb2d = GetComponent<Rigidbody2D>();
+        fsm = GetComponent<PlayMakerFSM>();
+    }
+
+    public	void AddForce (Vector2 force)
+   {
+        //消力
+        rgb2d.velocity = new Vector2(rgb2d.velocity.x, 0f );
+        rgb2d.AddForce( force);
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (isGameOver == true) return;
+            isGameOver = true;
+       
+            fsm.SendEvent( "GameOver");
+        
+
+    }
+}
---
+++ PlayerMaker_FSM_20220623_5.3.3f1/Assets/Scenes/第二章/Tags.cs
@@ -0,0 +1,5 @@
+﻿    public class Tags
+{
+    public const string Player = "Player";
+    public const string Obstacle = "Obstacle";
+}
